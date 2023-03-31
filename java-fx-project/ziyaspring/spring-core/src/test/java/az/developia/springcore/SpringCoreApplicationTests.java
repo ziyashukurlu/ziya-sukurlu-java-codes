e9e7c40e85b9cff62aad774d683b1873ad7d8ab3
Ziya <zshukurlu4727@gmail.com>
2023-03-31 14:54:40 +0400

    Spring core project created

---
+++ java-fx-project/ziyaspring/spring-core/src/test/java/az/developia/springcore/SpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
